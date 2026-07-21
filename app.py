fa3a7393260e466daf2b7254322809cf7bccf40d
JLCM1999 <joselalochavez02@gmail.com>
2026-07-21 13:29:01 -0600

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 
 st.set_page_config(page_title="Asistente de documentos", page_icon="📄", layout="wide")
